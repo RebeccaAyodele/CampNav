d70a6f021c2b3623bac857952f81e54aa7373ba0
Teminioluwa Adekoya <teminioluwaopemipo@gmail.com>
2026-06-24 08:59:53 +0100

    ,

--- k.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-body= {
-  "email": "admin@campnav.local",
-  "password": "ChangeMe123!"
-}
-res=requests.post("https://campnav.onrender.com/api/auth/login",json=body)
-
-print(res.json)
